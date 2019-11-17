613b48773602bf054aac4ce4078e6b7cae581934
Mattin <mattin-najafi@hotmail.com>
2019-11-17 11:32:36 +0100

    First official

---
+++ contact.js
@@ -0,0 +1,50 @@
+let today = new Date()
+let hour = today.getHours()
+
+
+function setBackground() {
+
+    if (hour <= 10 && hour >= 5) {
+        document.body.style.background = "linear-gradient(to bottom,#FF5F6D,#FFC371)"
+    }
+    else if (hour >= 11 && hour <= 17) {
+        document.body.style.background = "linear-gradient(to bottom,#005AA7,#b4dafa)"
+    }
+    else if (hour >= 18 && hour <= 23) {
+        document.body.style.background = " linear-gradient(to bottom,#000428,#01396b)"
+    }
+    else {
+        document.body.style.background = "linear-gradient(to bottom,#000000,#434343)"
+    }
+}
+
+function validate() {
+    let changeText = document.getElementById("describingText")
+    var wholeName = document.getElementById("name")
+    var mail = document.getElementById("mail")
+    var phoneNumber = document.getElementById("number")
+    var message = document.getElementById("msg")
+    var button = document.getElementById("sentMessage")
+    var header = document.getElementById("header")
+    
+    if (wholeName.value.trim() == "" || mail.value.trim() == "" || phoneNumber.value.trim() == "" 
+    || message.value.trim() == "" || message.value.trim() == null) {
+        changeText.innerHTML = "No blank values allowed."
+        return false
+    }
+    else{
+        changeText.innerHTML = "Thank you for your message. I will be in touch in 24h"
+        wholeName.style.display = "none"
+        mail.style.display = "none"
+        phoneNumber.style.display = "none"
+        message.style.display = "none"
+        header.style.display = "none"
+        button.style.display = "none"
+        changeText.style.fontSize = "2rem"
+        changeText.style.color = "#20f580"
+        changeText.style.fontWeight = "bolder"
+        return false
+    }
+}
+
+setBackground()
---
+++ script.js
@@ -0,0 +1,183 @@
+let today = new Date()
+let hour = today.getHours()
+let firstText = document.getElementById("frontText")
+window.addEventListener("scroll", scrolling)
+document.getElementById("logo").addEventListener("mouseover", logo)
+document.getElementById("personal").addEventListener("click", personal)
+document.getElementById("skills").addEventListener("click", skills)
+document.getElementById("projects1").addEventListener("mouseover", portfolioText1)
+document.getElementById("projects2").addEventListener("mouseover", portfolioText2)
+document.getElementById("projects3").addEventListener("mouseover", portfolioText3)
+document.getElementById("projects4").addEventListener("mouseover", portfolioText4)
+
+
+// document.onmousemove = mouseCircles;
+// function mouseCircles() {
+
+//     let circle = document.createElement("div")
+//     circle.setAttribute("id", "circles")
+//     document.body.appendChild(circle)
+
+//     circle.style.left = event.clientX + "px"
+//     circle.style.top = event.clientY + "px"
+
+//     circle.style.left = circle.offsetLeft - 15 + "px"
+//     circle.style.top = circle.offsetTop - 15 + "px"
+
+//     circle.style.transition = "all 0.7s linear 0s"
+//     circle.style.opacity = "0"
+
+//     circle.style.width = "40px"
+//     circle.style.height = "40px"
+//     circle.style.borderRadius = "30px"
+
+// }
+/**
+ * Setting my background depending on which time it is
+ */
+function setBackground() {
+    
+    if (hour <= 10 && hour >= 5) {
+        document.body.style.background = "linear-gradient(to bottom,#FF5F6D,#FFC371)"
+    }
+    else if (hour >= 11 && hour <= 17) {
+        document.body.style.background = "linear-gradient(to bottom,#005AA7,#FFFDE4)"
+    }
+    else if (hour >= 18 && hour <= 23) {
+        document.body.style.background = " linear-gradient(to bottom,#000428,#01396b)"
+    }
+    else {
+        document.body.style.background = "linear-gradient(to bottom,#000000,#434343)"
+    }
+}
+
+/**
+ * Start of changing text
+ */
+
+function firstFunction(){
+    setTimeout (secondText, 1000)
+}
+
+function secondText(){
+    firstText.style.transition = "opacity 1.5s linear 0s"
+    let opacity = firstText.style.opacity = "0";
+    setTimeout (replacingText, 1500)
+}
+
+function replacingText(){
+   let changeText = firstText.innerHTML.replace("Front End Developer" , "Just Kidding")
+    firstText.style.transition = "opacity 1.5s linear 0s"
+    firstText.style.opacity = "1";
+    document.getElementById("frontText").innerHTML = changeText
+    setTimeout(finalOpacity, 1500)
+
+}
+function finalOpacity(){
+    firstText.style.transition = "opacity 1.5s linear 0s"
+    let opacity = firstText.style.opacity = "0";
+    setTimeout (finalText,1500)
+}
+
+function finalText(){
+    let changeText = firstText.innerHTML.replace("Just Kidding" , "Soon Front End Developer")
+    firstText.style.transition = "opacity 2s linear 0s"
+    firstText.style.opacity = "1";
+    document.getElementById("frontText").innerHTML = changeText
+}
+
+/**
+ * My scrollarrow, whether its displayed or no, depending on my scroll
+ */
+function scrolling() {
+    if (window.scrollY != 0) {
+        document.getElementById("scrollArrow").style.transition = "opacity 1.5s linear 0s"
+        var opacity = document.getElementById("scrollArrow").style.opacity = "0"
+        if (opacity == 0) {
+            document.getElementById("scrollArrow").style.display = "none"
+        }
+    }
+
+    else {
+        document.getElementById("scrollArrow").style.transition = ""
+        document.getElementById("scrollArrow").style.opacity = ""
+        document.getElementById("scrollArrow").style.display = ""
+    }
+}
+
+/**
+ * Making the cursor, pointy on my logo
+ */
+function logo() {
+    document.getElementById("logo").style.cursor = "pointer"
+}
+
+
+/**
+ * Hiding the skillbox when you click on personal button, and showing personalbox
+ */
+function personal() {
+    document.getElementById("aboutMeSkills").style.display = "none"
+    document.getElementById("aboutMeText").style.display = "block"
+}
+/**
+ * Hiding the personalbox when you click on skills button, and showing skillsbox
+ */
+function skills() {
+    document.getElementById("aboutMeText").style.display = "none"
+    document.getElementById("aboutMeSkills").style.display = "block"
+
+
+}
+
+function portfolioText1() {
+    document.getElementById("imgOfProject1").style.display = "none"
+    document.getElementById("imgtext1").style.display = "flex"
+    document.getElementById("projects1").style.background = "white"
+
+
+}
+function portfolioText2() {
+    document.getElementById("imgOfProject2").style.display = "none"
+    document.getElementById("imgtext2").style.display = "flex"
+    document.getElementById("projects2").style.background = "white"
+
+
+}
+function portfolioText3() {
+    document.getElementById("imgOfProject3").style.display = "none"
+    document.getElementById("imgtext3").style.display = "flex"
+    document.getElementById("projects3").style.background = "white"
+
+}
+function portfolioText4() {
+
+    document.getElementById("imgOfProject4").style.display = "none"
+    document.getElementById("imgtext4").style.display = "flex"
+    document.getElementById("projects4").style.background = "white"
+
+}
+
+function portfolioimg1() {
+    document.getElementById("imgOfProject1").style.display = ""
+    document.getElementById("imgtext1").style.display = "none"
+    document.getElementById("projects1").style.background = ""
+
+}
+function portfolioimg2() {
+    document.getElementById("imgOfProject2").style.display = ""
+    document.getElementById("imgtext2").style.display = "none"
+    document.getElementById("projects2").style.background = ""
+}
+function portfolioimg3() {
+    document.getElementById("imgOfProject3").style.display = ""
+    document.getElementById("imgtext3").style.display = "none"
+    document.getElementById("projects3").style.background = ""
+}
+function portfolioimg4() {
+    document.getElementById("imgOfProject4").style.display = ""
+    document.getElementById("imgtext4").style.display = "none"
+    document.getElementById("projects4").style.background = ""
+}
+
+setBackground();
